0b8b68755ecce2283b91c4de7b78152648dc37df
Dylan Jackson <djackson@linfield.edu>
2023-05-08 13:29:16 -0700

    Added comments in the card views

--- Flashcards/Views/DeckCardBackView.swift
+++ Flashcards/Views/DeckCardBackView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct DeckCardBackView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // Create a variable that has the back value on it and then give the option to flip back over or
+        // Go to the next card
+        // if next is hit add 1 to the index count. Also add an if statement that checks to see if the index variable is greater than the decks array and if it is show a button that heads back to the decks view
     }
 }
 
--- Flashcards/Views/DeckCardFrontView.swift
+++ Flashcards/Views/DeckCardFrontView.swift
@@ -11,5 +11,15 @@ struct DeckCardFrontView: View {
     private var decks: [DeckModel] = []
     var body: some View {
         Text("Deck Card")
+        // create a variable that shows the front of the card and gives the option to flip over the card
+        // once it is flipped an option will shows up that allows you to go to the next card
+        // if next is hit add 1 to the index count. Also add an if statement that checks to see if the index variable is greater than the decks array and if it is show a button that heads back to the decks view
+    }
+}
+
+struct NextOrBack: View {
+    
+    var body: some View {
+        Text("eegegeggge")
     }
 }
